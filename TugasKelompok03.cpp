4b7628d8a7bab7dd3c22ba83669b29de663bf383
Mfajar22 <fajarmhmd2121@gmail.com>
2024-10-28 15:49:36 +0700

    Update TugasKelompok03.cpp

--- TugasKelompok03.cpp
+++ TugasKelompok03.cpp
@@ -3,12 +3,14 @@ using namespace std;
 
 void main() {
 	string kata;
+	string kataBalik;
 	cout << "---Check kata---\n\n";
 	cout << "Input kata : ";
 	cin >> kata;
 
-	string kataBalik = kata;
-	reverse(kataBalik.begin(), kataBalik.end());
+	for (int i = kata.length() - 1; i >= 0; i--) {
+		kataBalik += kata[i];
+	}
 
-	cout << "Hasil ; " << boolalpha << (kataBalik == kata);
+	cout << "Hasil : " << boolalpha << (kataBalik == kata);
 }
